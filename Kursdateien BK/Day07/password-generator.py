1c93c4bf8524d97a6c3c5da0095d8d269eed6fe2
Björn Kötter <dev@bk.netburn.de>
2021-12-20 18:30:49 +0100

    add user query password size and count

--- Kursdateien BK/Day07/password-generator.py
+++ Kursdateien BK/Day07/password-generator.py
@@ -3,18 +3,28 @@ import string
 
 
 def get_chars():
-    return string.ascii_letters + string.digits
+    return string.ascii_letters + string.digits + string.punctuation
 
 
-def get_password(chars: str, size: int):
-    return "".join(random.choice(chars) for x in range(size))
+def get_password(chars: str, opts: dict):
+    return "".join(random.choice(chars) for _ in range(opts['password_length']))
+
+
+def get_user_input():
+    return {
+        'password_length': int(input('Wie lang sollen die Passwörter sein? ')),
+        'password_count': int(input('Wie viele Passwörter sollen generiert werden? ')),
+    }
 
 
 def main():
-    size = 8
+    passwords = []
+    opts = get_user_input()
     chars = get_chars()
-    passw = get_password(chars, size)
-    print(passw)
+    for _ in range(opts['password_count']):
+        passwords.append(get_password(chars, opts))
+    for password in passwords:
+        print(password)
 
 
 if __name__ == '__main__':
